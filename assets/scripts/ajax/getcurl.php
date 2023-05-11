d5a3a372598cf57fc375c2110c8f649b1cba3943
fahmiirawan0803 <117510609+fahmiirawan0803@users.noreply.github.com>
2023-05-11 17:45:45 +0700

    first commit

---
+++ assets/scripts/ajax/getcurl.php
@@ -0,0 +1,39 @@
+<?php
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://farmasi.mimoapps.xyz/mimoqss2auyqD1EAlkgZCOhiffSsFl6QqAEIGtM");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+$output = curl_exec($curl);
+curl_close($curl);
+
+$data = json_decode($output, true);
+//echo "<pre>"; print_r($data); echo "</pre>";
+?>
+
+<html>
+    <head>
+        <title>Daftar Obat</title>
+    </head>
+    <body>
+        <h1>Data Obat</h1>
+        <table border="1" style="width: 100%">
+            <tr>
+                <td>Kode Barang</td>
+                <td>Nama Barang</td>
+                <td>Grup Barang</td>
+                <td>Harga Jual</td>
+            </tr>
+
+
+        <?php foreach($data as $obat) : ?>
+            <tr>
+                <td><?= $obat['i_code'] ?></td>
+                <td><?= $obat['i_name'] ?></td>
+                <td><?= $obat['g_code'] ?></td>
+                <td><?= $obat['i_sell'] ?></td>
+            </tr>
+
+        <?php endforeach ?>
+        </table>
+    </body>
+</html>
